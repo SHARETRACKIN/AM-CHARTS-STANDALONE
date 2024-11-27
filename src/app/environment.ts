b1c98b61800058e1bc1503c42dffb2edbdd7c68c
brenden@sharetrackin.com <brenden@sharetrackin.com>
2024-11-27 14:29:50 +0200

    updated the environment to Testing.

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
 
-  // apiUrl: 'https://ckadminmasterapitesting.sharetrackin.com:8446', // Testing
-  // apiAuthenticationEndpoint: 'https://ckadminmasterapitesting.sharetrackin.com:8446/api/AuthenticationEndpoint',
+  apiUrl: 'https://ckadminmasterapitesting.sharetrackin.com:8446', // Testing
+  apiAuthenticationEndpoint: 'https://ckadminmasterapitesting.sharetrackin.com:8446/api/AuthenticationEndpoint',
 
-  apiUrl: 'https://stasharetrackinapi.sharetrackin.com:8444', // Live
-  apiAuthenticationEndpoint: 'https://stasharetrackinapi.sharetrackin.com:8444/api/AuthenticationEndpoint',
+  // apiUrl: 'https://stasharetrackinapi.sharetrackin.com:8444', // Live
+  // apiAuthenticationEndpoint: 'https://stasharetrackinapi.sharetrackin.com:8444/api/AuthenticationEndpoint',
 
   // apiUrl: 'https://localhost:5003', // Testing
   // apiAuthenticationEndpoint: 'https://localhost:5003/api/AuthenticationEndpoint',
